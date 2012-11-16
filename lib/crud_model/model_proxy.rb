050ab19ab8ea1d2fc6c9024ffbd1ecaad6415afd
holysugar <holysugar@gmail.com>
2012-11-16 11:54:30 +0900

    remove debug line

--- lib/crud_model/model_proxy.rb
+++ lib/crud_model/model_proxy.rb
@@ -2,10 +2,6 @@
 module CrudModel
   module ModelProxy
     extend ActiveSupport::Concern
-    included do
-      p 'included!!!'
-    end
-
     def proxymodel(name)
       send(name)
     end
@@ -15,13 +11,10 @@ module CrudModel
         options = args.extract_options!
         raise ArgumentError, ":to is required" unless options[:to]
 
-        p args
         args += args.map{|attr| "#{attr}=".to_sym }
-        p args
         if options.delete(:primary) || delegations.blank?
           args += [:id, :persisted?, :new_record?]
         end
-        p args
         delegate *args, :to => options[:to]
         delegations[options[:to]] = args
       end
